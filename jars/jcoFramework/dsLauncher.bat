1d6a8e047cfaf03217c94771ff97f90826a8bd97
zunstraal <56400854+zunstraal@users.noreply.github.com>
2022-06-25 16:36:05 +0200

    Delete jars directory

--- jars/jcoFramework/dsLauncher.bat
+++
@@ -1,8 +0,0 @@
-
-: ds-server
-start cmd.exe --% /K jarLauncher 1
-
-timeout 3
-
-: ds-client
-start cmd.exe --% /K jarLauncher 2
--- jars/jcoFramework/uiLauncher.bat
+++
@@ -1,8 +0,0 @@
-
-: engine
-start cmd.exe --% /K jarLauncher 3
-
-timeout 3
-
-: ui
-start cmd.exe --% /K jarLauncher 4
